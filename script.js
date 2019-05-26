f50f870fd253433ee722470a7ad2f8dc9a8cab76
0ll0lll <0ll0lll@0ll0lll.local>
2019-05-25 20:01:53 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// Css Classes
+$('input').focus(function() {
+    $('.wrapper').addClass('active')
+})
+
+$('input').blur(function() {
+    $('.wrapper').removeClass('active')
+})
+
+// Events
+
+$('button').click(() => {
+    $('.text').html('')
+    requestData()
+    $('input').val('')
+})
+
+$('input').keydown((e) => {
+    if (e.which === 13) {
+        $('.text').html('')
+        requestData()
+        $('.wrapper').removeClass('active')
+        $('input').val('')
+    }
+})
+
+// AJAX )
+
+function requestData() {
+    let itemsNum = parseInt($('input').val()) || 1
+
+    $.getJSON('http://www.filltext.com/?rows=' + itemsNum + '&fname={firstName}&lname={lastName}&tel={phone|format}&address={streetAddress}&city={city}&state={usState|abbr}&zip={zip}&pretty=true', (data, b , c ) => {
+
+        console.log(c)
+        console.log('//////////////')
+        console.log(b)
+        console.log('//////////////')
+        console.log(data)
+
+        data.forEach((item, index) => {
+            $('.text').append('<p>' + '<span class="name">' + item.fname + '</span>' + ' lives at ' + item.address + ' ' + '<span class="zip">' + item.zip + '</span>' + '</p>')
+
+            if (index % 2 !== 0) {
+                $($('p')[index]).css('background-color', 'rgba(134, 70, 236, 0.18)')
+            }
+        })
+    })
+}
